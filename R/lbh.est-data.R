9d204f2b5b9ad48d341db00da119d116aa4bcbbd
maRce10 <marceloa27@gmail.com>
2020-02-07 08:31:03 -0600

    added vignette 2

--- R/lbh.est-data.R
+++ R/lbh.est-data.R
@@ -6,5 +6,8 @@
 #'   
 #' @description 'lbh.est' contains an \emph{extented selection table} following the warbleR package object format. The data contains the annotations and wave objects of 50 long-billed hermit songs from 10 different song types (5 per song type, 'lek.song.type' column) recorded at 8 leks ('lek' column) in the Caribbean slope of Costa Rica.
 #'   
+#' @references 
+#' Araya-Salas M, G Smith-vidaurre,  D Mennill, P Gonzalez-Gomez, J Cahill & T Wright. 2019. Social group signatures in hummingbird displays provide evidence of co-occurrence of vocal and visual learning. Proceedings of the Royal Society B. 286: 20190666
+#' 
 #' @usage data(lbh.est)
 "lbh.est" 
